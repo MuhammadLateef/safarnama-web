3a82c68266f73c4dd3947d1996b749fabea980f0
Muhammad Latif <96353895+MuhammadLateef@users.noreply.github.com>
2026-04-22 11:34:50 +0500

    Update footer company name and details

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -177,7 +177,7 @@ export default function Footer() {
                     <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"></path>
                     <circle cx="12" cy="10" r="3"></circle>
                   </svg>
-                  <span className="">SAFARNAMA TOURS PVT LTD, safarnama tours, airport road skardu, airport Rd, Skardu, Gilgit Baltistan
+                  <span className="">SAFARNAMA TOURS, airport road skardu, airport Rd, Skardu, Gilgit Baltistan
                     </span>
                 </li>
               </ul>
